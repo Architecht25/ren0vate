60c0db5a8ae4c296c6b63afcb48765f53721a3d1
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-07-02 15:23:46 +0200

    Remove Sprockets manifest initializer

--- config/initializers/sprockets_manifest.rb
+++
@@ -1,7 +0,0 @@
-# config/initializers/sprockets_manifest.rb
-
-Rails.application.config.assets.configure do |env|
-  # on force le manifest sur le répertoire public/assets, passé en String
-  manifest_path = Rails.root.join("public", "assets").to_s
-  env.instance_variable_set(:@manifest, Sprockets::Manifest.new(env, manifest_path))
-end
