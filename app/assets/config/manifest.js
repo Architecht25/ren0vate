6d33923277643981896e389650096fb62b3739ea
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-07-02 18:07:37 +0200

    still diving in truble water with js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,18 +1,4 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link application.js
-//= link bootstrap.min.js
-//= link popper.js
 
-  //= link controllers/categorie_estimation_controller.js
-  //= link controllers/region_display_controller.js
-  //= link controllers/region_selector_controller.js
-  //= link controllers/test_eligibilite_controller.js
-  //= link controllers/user_type_controller.js
-
-  //= link @hotwired--stimulus.js
-  //= link @hotwired--turbo-rails.js
-  //= link @hotwired--turbo.js
-
-    //= link controllers/application.js
-  //= link controllers/index.js
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,22 +1,8 @@
 import "@hotwired/turbo-rails"
-import "@popperjs/core"
 import "bootstrap"
 
 import { Application } from "@hotwired/stimulus"
+import { autoload } from "@hotwired/stimulus-loading"
 
-// Initialise Stimulus
 window.Stimulus = Application.start()
-
-// Contrôleurs Stimulus enregistrés manuellement
-import RegionSelectorController from "./controllers/region_selector_controller"
-import RegionDisplayController from "./controllers/region_display_controller"
-import UserTypeController from "./controllers/user_type_controller"
-import TestEligibiliteController from "./controllers/test_eligibilite_controller"
-import CategorieEstimationController from "./controllers/categorie_estimation_controller"
-
-// Enregistrement des contrôleurs
-window.Stimulus.register("region-selector", RegionSelectorController)
-window.Stimulus.register("region-display", RegionDisplayController)
-window.Stimulus.register("user-type", UserTypeController)
-window.Stimulus.register("test-eligibilite", TestEligibiliteController)
-window.Stimulus.register("categorie-estimation", CategorieEstimationController)
+autoload("controllers", window.Stimulus)
