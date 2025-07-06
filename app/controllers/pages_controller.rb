4328a5eafeef8df6dd819cfcb72db6c31ece62c4
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-07-06 11:04:54 +0200

    categorie 1, 2, 3, 4 checked OK. methode calcul cat 1 et 2 checked ok.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @primes = Prime.all
-    @categorie_id = 3 # ou toute logique que tu veux pour déterminer la catégorie en front
+    @categorie_id = 1 # ou toute logique que tu veux pour déterminer la catégorie en front
     @plafonds_par_categorie = Prime.group(:category_id).maximum(:plafond)
     @groupes_plafond = Prime.distinct.pluck(:groupe)
   end
