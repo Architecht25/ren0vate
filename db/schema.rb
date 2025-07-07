45ad55331b0f0baaf8980d99b11e51acf0648216
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-07-07 10:41:48 +0200

    login OK

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_07_075558) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_07_083939) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -204,6 +204,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_07_075558) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
