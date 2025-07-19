8b1722792c08502320e3254d7d7d91ce9cb8f69b
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-07-19 13:59:08 +0200

    fix: Add missing RegulatoryRequirementsController class definition

---
+++ app/controllers/regulatory_requirements_controller.rb
@@ -0,0 +1,13 @@
+class RegulatoryRequirementsController < ApplicationController
+  def index
+    # Vue principale des exigences réglementaires
+  end
+
+  def region_selection
+    # Sélection de la région
+  end
+
+  def ventilation_calculator
+    # Calculateur de ventilation
+  end
+end
