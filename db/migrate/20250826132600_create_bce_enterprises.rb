d23257ebc0851bb2df024705ed54ba22b89aec2e
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-08-26 20:11:16 +0200

    fix: Make BCE enterprises migration idempotent for Heroku deployment

--- db/migrate/20250826132600_create_bce_enterprises.rb
+++ db/migrate/20250826132600_create_bce_enterprises.rb
@@ -1,17 +1,26 @@
 class CreateBceEnterprises < ActiveRecord::Migration[8.0]
   def change
-    create_table :bce_enterprises do |t|
-      t.string :enterprise_number, null: false, index: { unique: true }
-      t.string :status_code
-      t.string :juridical_situation
-      t.string :type_of_enterprise
-      t.string :juridical_form
-      t.date :start_date
+    # Créer la table seulement si elle n'existe pas
+    unless table_exists?(:bce_enterprises)
+      create_table :bce_enterprises do |t|
+        t.string :enterprise_number, null: false, index: { unique: true }
+        t.string :status_code
+        t.string :juridical_situation
+        t.string :type_of_enterprise
+        t.string :juridical_form
+        t.date :start_date
 
-      t.timestamps
+        t.timestamps
+      end
     end
 
-    add_index :bce_enterprises, :status_code
-    add_index :bce_enterprises, :juridical_form
+    # Ajouter les index seulement s'ils n'existent pas
+    unless index_exists?(:bce_enterprises, :status_code)
+      add_index :bce_enterprises, :status_code
+    end
+    
+    unless index_exists?(:bce_enterprises, :juridical_form)
+      add_index :bce_enterprises, :juridical_form
+    end
   end
 end
