05273a7fe067d802a2a14e5221bd06dd3cf86ee1
ROBIN DU APRC <ROBIN@PRIMES-SERVICES.BE>
2025-08-26 19:03:40 +0200

    🔧 Fix migration BCE: ajout création table manquante
    - Ajout création table bce_enterprises si elle n'existe pas
    - Compatible avec environnements où la table existe ou pas
    - Migration sécurisée avec vérifications table_exists?
    - Résout erreur 'relation bce_enterprises does not exist' sur Heroku

--- db/migrate/20250826165919_fix_bce_enterprises_indexes.rb
+++ db/migrate/20250826165919_fix_bce_enterprises_indexes.rb
@@ -1,6 +1,24 @@
 class FixBceEnterprisesIndexes < ActiveRecord::Migration[8.0]
   def change
-    # La table existe déjà, on ajoute juste les index manquants si nécessaire
+    # Créer la table bce_enterprises si elle n'existe pas
+    unless table_exists?(:bce_enterprises)
+      create_table :bce_enterprises do |t|
+        t.string :enterprise_number, null: false
+        t.string :status
+        t.string :juridical_situation
+        t.string :type_of_enterprise
+        t.string :juridical_form
+        t.string :juridical_form_cac
+        t.date :start_date
+
+        t.timestamps
+      end
+    end
+
+    # Ajouter les index seulement s'ils n'existent pas
+    unless index_exists?(:bce_enterprises, :enterprise_number)
+      add_index :bce_enterprises, :enterprise_number, unique: true
+    end
 
     unless index_exists?(:bce_enterprises, :status)
       add_index :bce_enterprises, :status
