95ae13b34a2c821afde09fd86be0c156d529b61c
Obindu Arc <ROBIN@PRIMES-SERVICES.BE>
2025-11-22 17:32:49 +0100

    Fix: Formulaire Flandre - sections dynamiques JavaScript + champs techniques détaillés
    - Correction JavaScript show/hide pour sections travaux détaillées
    - Ajout champs techniques complets isolation (surface, méthode, date, matériau, marque)
    - Ajout champs chauffage, ventilation, travaux complémentaires
    - Mise à jour progress bar en pourcentage (64% complété)
    - Correction badges pré-remplissage avec fallback
    - Migration email tracking vers Request model
    - Conformité aux exigences officielles Mijn Verbouwpremie

--- app/services/request_steps_service.rb
+++ app/services/request_steps_service.rb
@@ -1,6 +1,8 @@
 class RequestStepsService
   def initialize(request)
     @request = request
+    @user = request.user
+    @property = request.property
   end
 
   def current_step
@@ -26,15 +28,41 @@ class RequestStepsService
   end
 
   def step_completion_percentage
-    case current_step
-    when 1 then 0
-    when 2 then 15
-    when 3 then 30
-    when 4 then 50
-    when 5 then 70
-    when 6 then 85
-    when 7 then 100
+    # Calcul plus granulaire basé sur les champs réellement manquants
+    total_sections = 6.0
+    completion = 0.0
+
+    # Section 1: Région (toujours complète si on arrive ici)
+    completion += 1.0
+
+    # Section 2: Informations de base
+    completion += 1.0 unless basic_info_incomplete?
+
+    # Section 3: Informations du bien (avec granularité)
+    if @request.flandre?
+      property_fields_total = 6.0 # adresse, cp, commune, type, usage, ean
+      property_fields_complete = 0.0
+      property_fields_complete += 1 unless get_field_value(:adresse, @property&.rue.present? && @property&.numero.present? ? "#{@property.rue} #{@property.numero}" : nil).blank?
+      property_fields_complete += 1 unless get_field_value(:code_postal, @property&.code_postal).blank?
+      property_fields_complete += 1 unless get_field_value(:commune, @property&.commune).blank?
+      property_fields_complete += 1 unless get_field_value(:type_bien, @property&.type_bien_flandre).blank?
+      property_fields_complete += 1 unless get_field_value(:usage, @property&.usage_flandre).blank?
+      property_fields_complete += 1 unless get_field_value(:code_ean, @property&.ean_flandre).blank?
+      completion += property_fields_complete / property_fields_total
+    else
+      completion += 1.0 unless property_info_incomplete?
     end
+
+    # Section 4: Détails des travaux
+    completion += 1.0 unless work_details_incomplete?
+
+    # Section 5: Informations financières
+    completion += 1.0 unless financial_info_incomplete?
+
+    # Section 6: Documents
+    completion += 1.0 unless documents_incomplete?
+
+    (completion / total_sections * 100).round
   end
 
   def can_advance_to_step?(step_number)
@@ -58,35 +86,48 @@ class RequestStepsService
 
   private
 
+  # Helper pour récupérer une valeur avec fallback (comme dans le formulaire)
+  def get_field_value(field_name, fallback_value = nil)
+    @request.form_data&.dig(field_name) || fallback_value || ""
+  end
+
   def basic_info_incomplete?
-    @request.title.blank? || @request.description.blank?
+    get_field_value(:nom, @user&.last_name).blank? ||
+    get_field_value(:prenom, @user&.first_name).blank? ||
+    get_field_value(:email, @user&.email).blank? ||
+    get_field_value(:telephone, @user&.phone).blank? ||
+    get_field_value(:registre_national, @user&.national_number).blank?
   end
 
   def property_info_incomplete?
     return false unless @request.flandre?
 
-    @request.adresse.blank? ||
-    @request.code_postal.blank? ||
-    @request.commune.blank? ||
-    @request.type_bien.blank?
+    get_field_value(:adresse, @property&.rue.present? && @property&.numero.present? ? "#{@property.rue} #{@property.numero}" : nil).blank? ||
+    get_field_value(:code_postal, @property&.code_postal).blank? ||
+    get_field_value(:commune, @property&.commune).blank? ||
+    get_field_value(:type_bien, @property&.type_bien_flandre).blank? ||
+    get_field_value(:usage, @property&.usage_flandre).blank? ||
+    get_field_value(:code_ean, @property&.ean_flandre).blank?
   end
 
   def work_details_incomplete?
     return false unless @request.flandre?
 
-    [
-      @request.travaux_toiture,
-      @request.travaux_murs,
-      @request.travaux_sol,
-      @request.travaux_vitrage,
-      @request.travaux_chauffage
-    ].all?(&:nil?)
+    # Vérifier s'il y a au moins un travaux sélectionné
+    travaux_fields = [:travaux_toiture, :travaux_murs, :travaux_sol, :travaux_vitrage, :travaux_chauffage, :travaux_ventilation, :travaux_complementaires]
+
+    travaux_fields.none? do |field|
+      value = get_field_value(field)
+      value == "true" || value == true || value == "1"
+    end
   end
 
   def financial_info_incomplete?
     return false unless @request.flandre?
 
-    @request.revenus_annuels.blank? || @request.personnes_charge.blank?
+    get_field_value(:revenus_annuels, @user&.household_income).blank? ||
+    get_field_value(:personnes_charge, @user&.nombre_enfants).blank? ||
+    get_field_value(:annee_aer, @user&.annee_revenus_demandeur).blank?
   end
 
   def documents_incomplete?
@@ -100,50 +141,72 @@ class RequestStepsService
   def flandre_missing_fields
     fields = []
 
-    # Informations personnelles
-    fields << "Nom" if @request.nom.blank?
-    fields << "Prénom" if @request.prenom.blank?
-    fields << "Email" if @request.email.blank?
-    fields << "Téléphone" if @request.telephone.blank?
-    fields << "Registre national" if @request.registre_national.blank?
+    # Informations de base (généralement pré-remplies)
+    fields << "Nom" if get_field_value(:nom, @user&.last_name).blank?
+    fields << "Prénom" if get_field_value(:prenom, @user&.first_name).blank?
+    fields << "Email" if get_field_value(:email, @user&.email).blank?
+    fields << "Téléphone" if get_field_value(:telephone, @user&.phone).blank?
+    fields << "Registre national" if get_field_value(:registre_national, @user&.national_number).blank?
 
-    # Informations du bien
-    fields << "Adresse" if @request.adresse.blank?
-    fields << "Code postal" if @request.code_postal.blank?
-    fields << "Commune" if @request.commune.blank?
-    fields << "Type de bien" if @request.type_bien.blank?
-    fields << "Usage" if @request.usage.blank?
+    # Informations du bien (généralement pré-remplies sauf code EAN)
+    fields << "Adresse" if get_field_value(:adresse, @property&.rue.present? && @property&.numero.present? ? "#{@property.rue} #{@property.numero}" : nil).blank?
+    fields << "Code postal" if get_field_value(:code_postal, @property&.code_postal).blank?
+    fields << "Commune" if get_field_value(:commune, @property&.commune).blank?
+    fields << "Type de bien" if get_field_value(:type_bien, @property&.type_bien_flandre).blank?
+    fields << "Usage" if get_field_value(:usage, @property&.usage_flandre).blank?
+    fields << "Code EAN" if get_field_value(:code_ean, @property&.ean_flandre).blank?
 
-    # Travaux
-    if work_details_incomplete?
+    # Travaux (souvent à sélectionner manuellement)
+    travaux_fields = [:travaux_toiture, :travaux_murs, :travaux_sol, :travaux_vitrage, :travaux_chauffage, :travaux_ventilation, :travaux_complementaires]
+    if travaux_fields.none? { |field| ["true", true, "1"].include?(get_field_value(field)) }
       fields << "Sélection des travaux"
     end
 
-    # Finances
-    fields << "Revenus annuels" if @request.revenus_annuels.blank?
-    fields << "Personnes à charge" if @request.personnes_charge.blank?
+    # Informations financières (généralement pré-remplies)
+    fields << "Revenus annuels" if get_field_value(:revenus_annuels, @user&.household_income).blank?
+    fields << "Personnes à charge" if get_field_value(:personnes_charge, @user&.nombre_enfants).blank?
+    fields << "Année AER" if get_field_value(:annee_aer, @user&.annee_revenus_demandeur).blank?
 
-    # Documents
-    fields << "Devis" unless @request.document_devis.attached?
-    fields << "Factures" unless @request.document_factures.attached?
-    fields << "AER" unless @request.document_aer.attached?
+    # Documents (requis)
+    fields << "Document devis" unless @request.document_devis.attached?
+    fields << "Document factures" unless @request.document_factures.attached?
+    fields << "Document AER" unless @request.document_aer.attached?
 
     fields
   end
 
   def bruxelles_missing_fields
     fields = []
-    fields << "Revenus du ménage" if @request.revenus_menage.blank?
-    fields << "Nombre de personnes" if @request.nombre_personnes.blank?
-    fields << "Type de travaux" if @request.type_travaux.blank?
+
+    # Informations de base
+    fields << "Nom" if get_field_value(:nom, @user&.last_name).blank?
+    fields << "Prénom" if get_field_value(:prenom, @user&.first_name).blank?
+    fields << "Email" if get_field_value(:email, @user&.email).blank?
+    fields << "Téléphone" if get_field_value(:telephone, @user&.phone).blank?
+
+    # Informations du bien
+    fields << "Adresse" if get_field_value(:adresse, @property&.adresse).blank?
+    fields << "Code postal" if get_field_value(:code_postal, @property&.code_postal).blank?
+    fields << "Commune" if get_field_value(:commune, @property&.commune).blank?
+
     fields
   end
 
   def wallonie_missing_fields
     fields = []
-    fields << "Revenus de référence" if @request.revenus_reference.blank?
-    fields << "Composition du ménage" if @request.composition_menage.blank?
-    fields << "Catégories de travaux" if @request.categories_travaux.blank?
+
+    # Informations de base
+    fields << "Nom" if get_field_value(:nom, @user&.last_name).blank?
+    fields << "Prénom" if get_field_value(:prenom, @user&.first_name).blank?
+    fields << "Email" if get_field_value(:email, @user&.email).blank?
+    fields << "Téléphone" if get_field_value(:telephone, @user&.phone).blank?
+
+    # Informations du bien
+    fields << "Adresse" if get_field_value(:adresse, @property&.adresse).blank?
+    fields << "Code postal" if get_field_value(:code_postal, @property&.code_postal).blank?
+    fields << "Commune" if get_field_value(:commune, @property&.commune).blank?
+
     fields
   end
+
 end
