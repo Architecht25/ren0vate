8256f11cdd8310e7ee53f67f477f69d42126b619
Obindu Arc <ROBIN@PRIMES-SERVICES.BE>
2026-05-24 15:32:38 +0200

    fix: before_action set_property manquant pour les actions vente

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,7 +2,7 @@ require 'net/http'
 
 class PropertiesController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_property, only: [:show, :dashboard, :edit, :update, :destroy, :purge_photo, :documents_dashboard, :peb_recommandations]
+  before_action :set_property, only: [:show, :dashboard, :edit, :update, :destroy, :purge_photo, :documents_dashboard, :peb_recommandations, :documents_phases_dashboard, :formulaire_miroir, :submit_prime, :select_form, :mise_en_vente, :activer_vente, :desactiver_vente, :marquer_vendu]
 
   def index
     @properties = current_user.properties
